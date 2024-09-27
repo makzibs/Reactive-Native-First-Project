b2b61c622fefaf622b473bf8f93a5ec499245d71
Anil Khanal <66034315+makzibs@users.noreply.github.com>
2024-09-28 02:01:22 +0300

    Project set up

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { HoverEffect } from "react-native-gesture-handler";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default Home;
